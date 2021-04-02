d18987f1e11393c011dcc64912da28c4fa727680
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2021-04-02 15:39:05 -0300

    Delete While.py

--- While.py
+++
@@ -1,9 +0,0 @@
-secret_number = "5"
-while True:
-    number_guess = input('Guess a number 1 to 5: ')
-    if number_guess == secret_number:
-        print('Yes!', number_guess, 'is correct!')
-        break
-    else:
-        print(number_guess, 'is incorrect')
-        
