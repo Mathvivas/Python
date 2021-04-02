c6bbbdc9d7224ac3e08701c6d78f35e2b39edd18
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2021-04-02 15:37:47 -0300

    Delete Arquivos Ímpares e Pares.py

--- Arquivos Ímpares e Pares.py
+++
@@ -1,9 +0,0 @@
-ímpares = open('Ímpares.txt', 'w')
-pares = open('Pares.txt', 'w')
-for n in range(0, 1000):
-    if n % 2 == 0:
-        pares.write('%d\n' % n)
-    else:
-        ímpares.write('%d\n' % n)
-pares.close()
-ímpares.close()
