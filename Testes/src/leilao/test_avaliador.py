837f7fcef2d123a5dfb9ac1a513939fa8076e59e
Mathvivas <mathvivas@hotmail.com>
2021-04-05 14:39:14 -0300

    Criação do Primeiro Teste

---
+++ Testes/src/leilao/test_avaliador.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from src.leilao.dominio import Usuario, Leilao, Lance, Avaliador
+
+
+class TestAvaliador(TestCase):
+    def test_avaliar(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avaliar(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
